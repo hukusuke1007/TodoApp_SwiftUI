317ef1107b4bfb47fee549c0d70f8ca949a2c5d0
Shohei Nakagawa <hukusuke1007@gmail.com>
2021-02-27 10:04:19 +0900

    added Firebase

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TodoApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TodoApp
+
+  pod 'Firebase/Firestore'
+  pod 'FirebaseFirestoreSwift'
+
+  target 'TodoAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TodoAppUITests' do
+    # Pods for testing
+  end
+
+end
--- TodoApp/TodoAppApp.swift
+++ TodoApp/TodoAppApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TodoAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+ 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
